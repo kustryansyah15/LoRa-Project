31fc173c1a832134b906d6af0ac55db3d5f3605b
MFKust <111276355+kustryansyah15@users.noreply.github.com>
2024-05-19 17:24:23 +0700

    Update secrets.h

--- 1. GATEWAY/demo_gateway_n/secrets.h
+++ 1. GATEWAY/demo_gateway_n/secrets.h
@@ -1,11 +1,11 @@
 //Wifi Credential
-#define WIFI_SSID     "adinda" 
-#define WIFI_PASS     "password" 
+#define WIFI_SSID     "XXX" 
+#define WIFI_PASS     "XXX" 
 
 //MQTT Details
-#define MQTT_SERVER   "103.156.114.74"
-#define MQTT_PORT     30425
-#define MQTT_USERNAME "adminlcs"
-#define MQTT_PASS     "adminlcs123"
-#define DEVICE_ID     "node01"
-#define TOPIC_BASE    "LORA-IOT/"
+#define MQTT_SERVER   "XXX"
+#define MQTT_PORT     XXX
+#define MQTT_USERNAME "XXX"
+#define MQTT_PASS     "XXX"
+#define DEVICE_ID     "XXX"
+#define TOPIC_BASE    "XXX"
