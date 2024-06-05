1efacc4956f1246564dbe611a5abf7bf98b08bc1
MFKust <111276355+kustryansyah15@users.noreply.github.com>
2024-06-05 21:41:07 +0700

    Update database.js

--- 4. BE_DB/be-loraiot-main/backendWeb/src/configs/database.js
+++ 4. BE_DB/be-loraiot-main/backendWeb/src/configs/database.js
@@ -1,7 +1,7 @@
 module.exports = {
   multipleStatements: true,
   host: 'localhost',
-  user: 'mfkk',
-  password: 'Mfkk123!',
-  database: 'loraiot'
+  user: 'username',
+  password: 'password',
+  database: 'database'
 };
